43574fd525fe5c165ded4cc4e229e4ed0de93aeb
Alexander Mylnikov <sasha.mylnikov@gmail.com>
2015-11-16 08:28:47 +0200

    Added dml statements.

--- dml.sql
+++ dml.sql
@@ -1,131 +1,131 @@
-insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE, CREATED, LASTUPDATED) 
+insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE,ISKITCHEN, CREATED, LASTUPDATED) 
 	values 
 		('Test Dish 2', 
 		50, 
 		15, 
 		'https://static-s.aa-cdn.net/img/ios/1011678946/a52d066d2dcac95260eff8b8b30580ff?v=1',
-		true,
+		true,false,
 		current_timestamp, 
 		current_timestamp);
 		
-insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE, CREATED, LASTUPDATED) 
+insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE,ISKITCHEN, CREATED, LASTUPDATED) 
 	values 
 		('Test Dish 3', 
 		50, 
 		15, 
 		'https://static-s.aa-cdn.net/img/ios/1011678946/a52d066d2dcac95260eff8b8b30580ff?v=1',
-		true,
+		true,true,
 		current_timestamp, 
 		current_timestamp);
 		
 		
-insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE, CREATED, LASTUPDATED) 
+insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE,ISKITCHEN, CREATED, LASTUPDATED) 
 	values 
 		('Test Dish 4', 
 		50, 
 		15, 
 		'https://static-s.aa-cdn.net/img/ios/1011678946/a52d066d2dcac95260eff8b8b30580ff?v=1',
-		true,
+		true,true,
 		current_timestamp, 
 		current_timestamp);
 		
 		
-insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE, CREATED, LASTUPDATED) 
+insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE,ISKITCHEN, CREATED, LASTUPDATED) 
 	values 
 		('Test Dish 5', 
 		50, 
 		15, 
 		'https://static-s.aa-cdn.net/img/ios/1011678946/a52d066d2dcac95260eff8b8b30580ff?v=1',
-		true,
+		true,true,
 		current_timestamp, 
 		current_timestamp);
 		
 		
-insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE, CREATED, LASTUPDATED) 
+insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE,ISKITCHEN, CREATED, LASTUPDATED) 
 	values 
 		('Test Dish 6', 
 		50, 
 		15, 
 		'https://static-s.aa-cdn.net/img/ios/1011678946/a52d066d2dcac95260eff8b8b30580ff?v=1',
-		true,
+		true,true,
 		current_timestamp, 
 		current_timestamp);
 		
 		
 		
-insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE, CREATED, LASTUPDATED) 
+insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE,ISKITCHEN, CREATED, LASTUPDATED) 
 	values 
 		('Test Dish 7', 
 		50, 
 		15, 
 		'https://static-s.aa-cdn.net/img/ios/1011678946/a52d066d2dcac95260eff8b8b30580ff?v=1',
-		true,
+		true,true,
 		current_timestamp, 
 		current_timestamp);
 		
 		
 		
 		
-insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE, CREATED, LASTUPDATED) 
+insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE,ISKITCHEN, CREATED, LASTUPDATED) 
 	values 
 		('Test Dish 8', 
 		50, 
 		15, 
 		'https://static-s.aa-cdn.net/img/ios/1011678946/a52d066d2dcac95260eff8b8b30580ff?v=1',
-		true,
+		true,true,
 		current_timestamp, 
 		current_timestamp);
 		
 		
-insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE, CREATED, LASTUPDATED) 
+insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE,ISKITCHEN, CREATED, LASTUPDATED) 
 	values 
 		('Test Dish 9', 
 		50, 
 		15, 
 		'https://static-s.aa-cdn.net/img/ios/1011678946/a52d066d2dcac95260eff8b8b30580ff?v=1',
-		true,
+		true,true,
 		current_timestamp, 
 		current_timestamp);
 		
-insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE, CREATED, LASTUPDATED) 
+insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE,ISKITCHEN, CREATED, LASTUPDATED) 
 	values 
 		('Test Dish 10', 
 		50, 
 		15, 
 		'https://static-s.aa-cdn.net/img/ios/1011678946/a52d066d2dcac95260eff8b8b30580ff?v=1',
-		true,
+		true,false,
 		current_timestamp, 
 		current_timestamp);
 		
 		
-insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE, CREATED, LASTUPDATED) 
+insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE,ISKITCHEN, CREATED, LASTUPDATED) 
 	values 
 		('Test Dish 11', 
 		50, 
 		15, 
 		'https://static-s.aa-cdn.net/img/ios/1011678946/a52d066d2dcac95260eff8b8b30580ff?v=1',
-		true,
+		true,false,
 		current_timestamp, 
 		current_timestamp);
 		
 		
-insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE, CREATED, LASTUPDATED) 
+insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE,ISKITCHEN, CREATED, LASTUPDATED) 
 	values 
 		('Test Dish 12', 
 		50, 
 		15, 
 		'https://static-s.aa-cdn.net/img/ios/1011678946/a52d066d2dcac95260eff8b8b30580ff?v=1',
-		true,
+		true,false,
 		current_timestamp, 
 		current_timestamp);
 		
 		
-insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE, CREATED, LASTUPDATED) 
+insert into PRODUCT(NAME, PRICE, QUANTITY, IMAGE_URL, ISONLINE,ISKITCHEN, CREATED, LASTUPDATED) 
 	values 
 		('Test Dish 13', 
 		50, 
 		15, 
 		'https://static-s.aa-cdn.net/img/ios/1011678946/a52d066d2dcac95260eff8b8b30580ff?v=1',
-		true,
+		true,false,
 		current_timestamp, 
 		current_timestamp);
